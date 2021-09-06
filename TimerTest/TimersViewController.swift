bacb76b59e6f97c7380f14c24daf9046db2d2475
Anastasia <goryachevskaya.nastya@yandex.ru>
2021-09-06 16:33:47 +0300

    add links to storyboard

---
+++ TimerTest/TimersViewController.swift
@@ -0,0 +1,22 @@
+//
+//  TimersViewController.swift
+//  TimerTest
+//
+//  Created by Анастасия Горячевская on 06.09.2021.
+//
+
+import UIKit
+
+class TimersViewController: UIViewController {
+
+    @IBOutlet weak var timerNameTextField: UITextField!
+    @IBOutlet weak var timerTimePickerView: UIPickerView!
+    @IBOutlet weak var addTimerButton: UIButton!
+    @IBOutlet weak var currentTimersTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
